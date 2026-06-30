7e64fa6d708ad4e995bd2d4ecf5e024fe407b2b0
Musa <musatasoluk0@gmail.com>
2026-06-30 11:01:16 +0200

    API fonctionnelle - tests validés

--- src/main/java/edu/ban7/bsd_eval/entity/AppUser.java
+++ src/main/java/edu/ban7/bsd_eval/entity/AppUser.java
@@ -1,11 +1,23 @@
 package edu.ban7.bsd_eval.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "app_user")
@@ -21,7 +33,7 @@ public class AppUser {
     @Column(unique = true, nullable = false)
     private String email;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(nullable = false)
     private String password;
 
